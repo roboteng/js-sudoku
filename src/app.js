5f7791266820635d2203a2130df0be2e15a7c18a
Trevor Settles <roboteng@gmail.com>
2021-07-25 19:25:23 -0600

    added sample sudoku puzzle

--- src/app.js
+++ src/app.js
@@ -1,11 +1,5 @@
 var setup = function () {
-  puzzle = state.puzzle;
-  puzzle.numbers = puzzle.numbers.map(function(elem, i) {return new GivenCell(i);});
-  puzzle.numbers[1] = new SolvedCell(2);
-  puzzle.numbers[2] = new OptionCell();
-  puzzle.numbers[2].options[3] = true;
-  puzzle.numbers[2].options[5] = true;
-  puzzle.numbers[3] = new OptionCell();
+  state.puzzle = sampleSudoku();
   display(state);
 };
 
--- src/domain/logic.js
+++ src/domain/logic.js
@@ -14,3 +14,46 @@ var createDigitButtonCallback = function (state, digit) {
     display(state);
   }
 }
+
+var sampleSudoku = function () {
+  var puzzle = new Sudoku();
+  for (var i = 0; i < 81; i++){
+    puzzle.numbers[i] = new OptionCell();
+  }
+  puzzle.setCell(2, 0, new GivenCell(3));
+  puzzle.setCell(4, 0, new GivenCell(2));
+  puzzle.setCell(5, 0, new GivenCell(7));
+  puzzle.setCell(7, 0, new GivenCell(4));
+  puzzle.setCell(1, 1, new GivenCell(6));
+  puzzle.setCell(2, 1, new GivenCell(1));
+  puzzle.setCell(3, 1, new GivenCell(3));
+  puzzle.setCell(5, 1, new GivenCell(9));
+  puzzle.setCell(0, 2, new GivenCell(4));
+  puzzle.setCell(1, 2, new GivenCell(9));
+  puzzle.setCell(4, 2, new GivenCell(6));
+  puzzle.setCell(6, 2, new GivenCell(7));
+  puzzle.setCell(0, 3, new GivenCell(8));
+  puzzle.setCell(2, 3, new GivenCell(4));
+  puzzle.setCell(8, 3, new GivenCell(9));
+  puzzle.setCell(1, 4, new GivenCell(2));
+  puzzle.setCell(2, 4, new GivenCell(7));
+  puzzle.setCell(4, 4, new GivenCell(8));
+  puzzle.setCell(6, 4, new GivenCell(4));
+  puzzle.setCell(7, 4, new GivenCell(6));
+  puzzle.setCell(0, 5, new GivenCell(9));
+  puzzle.setCell(6, 5, new GivenCell(8));
+  puzzle.setCell(8, 5, new GivenCell(2));
+  puzzle.setCell(2, 6, new GivenCell(8));
+  puzzle.setCell(4, 6, new GivenCell(9));
+  puzzle.setCell(7, 6, new GivenCell(5));
+  puzzle.setCell(8, 6, new GivenCell(1));
+  puzzle.setCell(3, 7, new GivenCell(5));
+  puzzle.setCell(5, 7, new GivenCell(1));
+  puzzle.setCell(6, 7, new GivenCell(2));
+  puzzle.setCell(7, 7, new GivenCell(8));
+  puzzle.setCell(1, 8, new GivenCell(1));
+  puzzle.setCell(3, 8, new GivenCell(2));
+  puzzle.setCell(4, 8, new GivenCell(7));
+  puzzle.setCell(6, 8, new GivenCell(6));
+  return puzzle;
+};
