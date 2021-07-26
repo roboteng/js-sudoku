57001194b294e84b494df1593c6307eddb648f2e
Trevor Settles <roboteng@gmail.com>
2021-07-25 19:42:45 -0600

    added tracking for shift key

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 var setup = function () {
   state.puzzle = sampleSudoku();
+
+  $(document).on('keydown keyup',  function (event) {
+    state.shiftHeld = event.shiftKey;
+    console.log('shift is: ' + state.shiftHeld);
+  });
+
   display(state);
 };
 
-var state = {pencilMode: false, selectedCells: [], puzzle: new Sudoku()};
+var state = {
+  pencilMode: false,
+  selectedCells: [],
+  puzzle: new Sudoku(),
+  shiftHeld: false
+};
 
-$('document').ready(function () {
-  setup();
-});
+$(document).ready(setup);
